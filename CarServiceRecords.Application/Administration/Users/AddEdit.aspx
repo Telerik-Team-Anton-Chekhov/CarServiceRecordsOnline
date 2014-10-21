5892ee9a403f64ece1a57ee0a7f0cf286e8ac306
Dimitar Kostov <d.kostov.dev@gmail.com>
2014-10-21 16:15:44 +0300

    Added Users List/Delete

---
+++ CarServiceRecords.Application/Administration/Users/AddEdit.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Administration/Admin.Master" AutoEventWireup="true" CodeBehind="AddEdit.aspx.cs" Inherits="CarServiceRecords.Application.Administration.Users.AddEdit" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="AdminContent" runat="server">
+</asp:Content>
---
+++ CarServiceRecords.Application/Administration/Users/List.aspx.cs
@@ -0,0 +1,38 @@
+﻿using CarServiceRecords.Common;
+using CarServiceRecords.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CarServiceRecords.Application.Administration.Users
+{
+    public partial class List : System.Web.UI.Page
+    {
+        private static IDataProvider data;
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            data = new DataProvider();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                this.ItemsList.DataSource = data.Users.All().ToList();
+                this.ItemsList.DataBind();
+            }
+        }
+
+        public void Delete_Item(object sender, CommandEventArgs e)
+        {
+            var item = data.Users.Find(e.CommandArgument.ToString());
+            data.Users.Delete(item);
+            data.SaveChanges();
+            Response.Redirect(Request.RawUrl);
+        }
+    }
+}
